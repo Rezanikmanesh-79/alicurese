fdbdd20e776900b6e7713d2f4f64d848ebc0cb66
Reza Nikmanesh <rezanikmanesh79@gmail.com>
2025-04-27 17:29:53 +0330

    adding login jwt to locust

--- core/core/locust/locustfile.py
+++ core/core/locust/locustfile.py
@@ -1,4 +1,19 @@
-from locust import HttpUser, task, between
+from locust import HttpUser, task
 
-class QuickstartUser(HttpUser):
-    pass
+class HelloWorldUser(HttpUser):
+    def on_start(self):
+        response = self.client.post("/accounts/api/v2/jwt/create/", json={
+            "email": "admin@admin.com",
+            "password": "1234"
+        }).json()
+        token = response.get("access")
+        if token:
+            self.client.headers.update({'Authorization': f'Bearer {token}'})
+
+    @task
+    def post_list(self):
+        self.client.get("/blog/api/v1/post/")
+
+    @task
+    def category_list(self):
+        self.client.get("/blog/api/v1/catgory/")
