6145f099abbc671dd48777d47b2d757ab4bb436c
Reza Nikmanesh <rezanikmanesh79@gmail.com>
2025-04-30 10:16:02 +0330

    using cache decorator

--- core/accounts/views.py
+++ core/accounts/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import time
 from accounts.tasks import sendEmail
-
+from django.views.decorators.cache import cache_page
 
 def send_email(request):
     sendEmail.delay()
     return HttpResponse("<h1>Done sending</h1>")
+
+# @cache_page(60)
